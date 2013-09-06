7cc7b6c4de2f588cbe2f1bc4031e8c280482b4d5
Jason Alvis <alvis.jason@gmail.com>
2013-09-06 21:02:28 +0100

    Updated for performance / mobile optimistation
    Removed unnecessary js from within local.xml
    Only one js file for custom code is now used.
    Moved jquery.1.7.2.min.js into skin directory.
    Moved jquery.1.7.2.min.js to load at the bottom of the dom for better
    performance.

---
+++ skin/frontend/mypackagename/default/js/jquery.run.js
@@ -0,0 +1,14 @@
+/**
+* Website name here
+* Website URL here
+*
+* @author Author name here
+* @package mypackagename_default
+*
+*/ 
+(function ($) {		
+    $(document).ready(function(){							
+    
+    	
+    });
+})(jQuery);
