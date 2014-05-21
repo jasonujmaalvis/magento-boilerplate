32b9d4f1ef064a05d40610bdfb73d056af2e3045
Jason Alvis <hello@jasonalvis.co.uk>
2014-05-21 21:14:35 +0100

    naming convention fixes

--- skin/frontend/mypackagename/default-config/config.rb
+++ skin/frontend/mypackagename/default-config/config.rb
@@ -1,5 +1,5 @@
 # note: this should never truly be refernced since we are using relative assets
-http_path = "/skin/frontend/rwd-config/default/"
+http_path = "/skin/frontend/mypackagename/default-config/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
