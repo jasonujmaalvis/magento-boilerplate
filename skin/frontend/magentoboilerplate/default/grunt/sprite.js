0ba17c4a3d2291e5868e75eeb3a773dbdc8063c9
Jason Alvis <hello@jasonalvis.co.uk>
2015-07-26 18:41:10 +0100

    update package.json / sprite to work with latest version

--- skin/frontend/magentoboilerplate/default/grunt/sprite.js
+++ skin/frontend/magentoboilerplate/default/grunt/sprite.js
@@ -1,20 +1,20 @@
 module.exports = {
-    core: {
-        src: 'images/sprites/core-src/**/*.*',
-        destImg: 'images/sprites/sprite-core.png',
-        destCSS: 'scss/utilities/sprites/_sprite-core.scss',
-        imgPath: '../images/sprites/sprite-core.png',
+    build: {
+        src: 'images/sprites/source/**/*.*',
+        dest: 'images/sprites/sprite.png',
+        destCss: 'scss/utilities/sprites/_sprite.scss',
+        imgPath: '../images/sprites/sprite.png',
         cssTemplate: 'scss/utilities/sprites.styl.mustache',
         cssVarMap: function (sprite) {
             sprite.name = 'sprite__' + sprite.name;
         },
         padding: 20
     },
-    core_2x: {
-        src: 'images/sprites/core-src-2x/**/*.*',
-        destImg: 'images/sprites/sprite-core-2x.png',
-        destCSS: 'scss/utilities/sprites/_sprite-core-2x.scss',
-        imgPath: '../images/sprites/sprite-core-2x.png',
+    buildRetina: {
+        src: 'images/sprites/source-2x/**/*.*',
+        dest: 'images/sprites/sprite-2x.png',
+        destCss: 'scss/utilities/sprites/_sprite-2x.scss',
+        imgPath: '../images/sprites/sprite-2x.png',
         cssTemplate: 'scss/utilities/sprites.styl.mustache',
         cssVarMap: function (sprite) {
             sprite.name = 'sprite-2x__' + sprite.name;
